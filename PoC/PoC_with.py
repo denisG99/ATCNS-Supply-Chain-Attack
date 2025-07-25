11a87f60e1e749f8950152ec55c7d2704d5c93cb
denisG99 <gasparollodenis@gmail.com>
2025-07-25 23:44:45 +0200

    FIX PoC that use with

--- PoC/PoC_with.py
+++ PoC/PoC_with.py
@@ -1,15 +1,15 @@
 import contextlib
-import __main__ # we need this because we use functions defines into the main script
+import __main__ # we need this because we use functions defines into the main script, but could be any module where the target function is defined
 
 @contextlib.contextmanager
 def override_function(target_module, func_name, new_func):
-    old_func = getattr(target_module, func_name)
-    setattr(target_module, func_name, new_func)
+    old_func = getattr(target_module, func_name) # storing temporarily the old function
+    setattr(target_module, func_name, new_func) # overwrite the old function with the new one
 
     try:
         yield
     finally:
-        setattr(target_module, func_name, old_func)
+        setattr(target_module, func_name, old_func) # restoring old function
 
 def computation(a, b):
     return a ** b
@@ -21,6 +21,7 @@ def malicious_computation(a, b):
     return a ** b
 
 if __name__ == '__main__':
+    # this tecnique is often use to mocking, testing and libraries override
     print(f"Benign computation {computation(2, 3)}")
 
     with override_function(__main__, "computation", malicious_computation):
