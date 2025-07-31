4373b1df05331911c7e87a81d7d2f1b47a32cda4
denisG99 <gasparollodenis@gmail.com>
2025-07-31 23:55:57 +0200

    FIX parent relation between scopes (from saving children, we save only parent node)

--- src/scope_graph.py
+++ src/scope_graph.py
@@ -22,7 +22,7 @@ class ScopeGraph(ast.NodeVisitor):
 
     def __init__(self) -> None:
         self.__scope_stack: list[str] = ["s0__main__"] # global scope
-        self.__graph: dict = {"s0__main__": {"decls": set(), "refs": set(), "children": []}}
+        self.__graph: dict = {"s0__main__": {"decls": set(), "refs": set(), "parent": None}}
         self.__next_id: int = 1
 
     def get_graph(self) -> dict:
@@ -37,22 +37,21 @@ class ScopeGraph(ast.NodeVisitor):
         """
         return self.__scope_stack[-1]
 
-    #def parent_scope(self) -> str:
-     #   return self.__scope_stack[-2] if len(self.__scope_stack) > 1 else None
-
-    #def __define_scope(self, node: Any) -> str:
-     #   pass
+    def parent_scope(self) -> str | None:
+        return self.__scope_stack[-2] if len(self.__scope_stack) > 1 else None
 
     def visit_FunctionDef(self, node:ast.FunctionDef) -> None:
         sid = f"s{self.__next_id}_func_{node.name}"
 
         self.__next_id += 1
-        self.__graph[sid] = {"decls": set(), "refs": set(), "children": []}
+        self.__graph[sid] = {"decls": set(), "refs": set(), "parent": None}
 
         self.__graph[self.__current_scope()]["decls"].add(node.name)
-        self.__graph[self.__current_scope()]["children"].append(sid)
 
         self.__scope_stack.append(sid)
+
+        if (parent := self.parent_scope())is not None:
+            self.__graph[self.__current_scope()]["parent"] = parent
 
         for arg in node.args.args:
             self.__graph[sid]["decls"].add(arg.arg)
@@ -64,24 +63,29 @@ class ScopeGraph(ast.NodeVisitor):
         sid = f"s{self.__next_id}_lambda"
 
         self.__next_id += 1
-        self.__graph[sid] = {"decls": set(), "refs": set(), "children": []}
+        self.__graph[sid] = {"decls": set(), "refs": set(), "parent": None}
 
-        self.__graph[self.__current_scope()]["children"].append(sid)
+        self.__scope_stack.append(sid)
+
+        if (parent := self.parent_scope())is not None:
+            self.__graph[sid]["parent"] = parent
 
         for arg in node.args.args:
             self.__graph[sid]["decls"].add(arg.arg)
 
         self.generic_visit(node)
+        self.__scope_stack.pop()
 
     def visit_ClassDef(self, node: ast.ClassDef) -> None:
         sid = f"s{self.__next_id}_class_{node.name}"
 
         self.__next_id += 1
-        self.__graph[sid] = {"decls": set(), "refs": set(), "children": []}
-
-        self.__graph[self.__current_scope()]["children"].append(sid)
+        self.__graph[sid] = {"decls": set(), "refs": set(), "parent": None}
 
         self.__scope_stack.append(sid)
+
+        if (parent := self.parent_scope())is not None:
+            self.__graph[self.__current_scope()]["parent"] = parent
 
         self.generic_visit(node)
         self.__scope_stack.pop()
@@ -90,21 +94,26 @@ class ScopeGraph(ast.NodeVisitor):
         sid = f"s{self.__next_id}_lstComp"
 
         self.__next_id += 1
-        self.__graph[sid] = {"decls": set(), "refs": set(), "children": []}
+        self.__graph[sid] = {"decls": set(), "refs": set(), "parent": None}
 
-        self.__graph[self.__current_scope()]["children"].append(sid)
+        self.__scope_stack.append(sid)
+
+        if (parent := self.parent_scope())is not None:
+            self.__graph[sid]["parent"] = parent
 
         self.generic_visit(node)
+        self.__scope_stack.pop()
 
     def visit_ExceptHandler(self, node: ast.ExceptHandler) -> None:
         sid = f"s{self.__next_id}_excHandler"
 
         self.__next_id += 1
-        self.__graph[sid] = {"decls": set(), "refs": set(), "children": []}
-
-        self.__graph[self.__current_scope()]["children"].append(sid)
+        self.__graph[sid] = {"decls": set(), "refs": set(), "parent": None}
 
         self.__scope_stack.append(sid)
+
+        if (parent := self.parent_scope())is not None:
+            self.__graph[self.__current_scope()]["parent"] = parent
 
         if node.name is not None:
             self.__graph[sid]["decls"].add(node.name)
@@ -116,12 +125,14 @@ class ScopeGraph(ast.NodeVisitor):
         sid = f"s{self.__next_id}_asyncFunc_{node.name}"
 
         self.__next_id += 1
-        self.__graph[sid] = {"decls": set(), "refs": set(), "children": []}
+        self.__graph[sid] = {"decls": set(), "refs": set(), "parent": None}
 
         self.__graph[self.__current_scope()]["decls"].add(node.name)
-        self.__graph[self.__current_scope()]["children"].append(sid)
 
         self.__scope_stack.append(sid)
+
+        if (parent := self.parent_scope())is not None:
+            self.__graph[self.__current_scope()]["parent"] = parent
 
         for arg in node.args.args:
             self.__graph[sid]["decls"].add(arg.arg)
