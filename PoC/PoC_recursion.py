fe67a434c737d9b354983f55b9d47ec599280748
denisG99 <gasparollodenis@gmail.com>
2025-08-10 16:14:43 +0200

    ADD recursion PoC

---
+++ PoC/PoC_recursion.py
@@ -0,0 +1,16 @@
+def factorial(n):
+    def factorial(n):
+        print("make some bad stuff!")
+
+        if n == 1:
+            return 1
+        else:
+            return n * factorial(n - 1)
+
+    if n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+if __name__ == '__main__':
+    print(factorial(5))
