51367dd006d17b790abc34f19f7bf7860d28ce03
denisG99 <gasparollodenis@gmail.com>
2025-08-28 16:48:48 +0200

    ADD evaluation sample

---
+++ src/evaluation/sample.py
@@ -0,0 +1,13 @@
+# the goal of this code is to evaluate and shows how scope graph and detector work, so it is not intended to be executed
+def g():
+    ...
+
+def f():
+    x = 1
+
+    def g():
+        x  = 2
+        #return x + y
+        ...
+
+    return g()
