b57c1be13fc1d1bd35ca889a6a4e3a651b46de4d
denisG99 <gasparollodenis@gmail.com>
2025-08-30 00:29:59 +0200

    MISC #6

--- src/evaluation/eval.py
+++ src/evaluation/eval.py
@@ -1,6 +1,6 @@
 from src.detector import Detector
 
 if __name__ =="__main__":
-    detector = Detector("./sample.py", "eval")
+    detector = Detector("./sample_class.py", "class-eval")
 
     detector.shadowing_detection()
