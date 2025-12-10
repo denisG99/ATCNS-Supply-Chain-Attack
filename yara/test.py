39f05372661a8b94c3e575440cd7a0666687af9a
denisG99 <gasparollodenis@gmail.com>
2025-12-10 13:22:07 +0100

    test file updated: context manager decorator and with statement rule tests

--- yara/test.py
+++ yara/test.py
@@ -1,4 +1,22 @@
 from unittest.mock import patch
+from contextlib import contextmanager
+
+import os
+
+@patch("os.listdir")
+def test():
+    assert "test1" == os.listdir()
+
+
+@contextmanager
+def ContextManager():
+    # Before yield as the enter method
+    print("Enter method called")
+    yield
+
+    # After yield as the exit method
+    print("Exit method called")
 
 if __name__ == "__main__":
-    ...
+    with ContextManager() as ctx:
+        ...
