e4bbc7f763631da78384b9621b522b1694013042
denisG99 <gasparollodenis@gmail.com>
2025-12-16 11:38:13 +0100

    MISC #9

--- yara/main.py
+++ yara/main.py
@@ -1,8 +1,12 @@
 import yara
+import os
 
 if __name__ == "__main__":
     var1 = "prova"
     var2 = "prova"
+
+    rules.append(yara.compile("./rules/monkey-patching.yar"))
+    rules.append(yara.compile("./rules/variables.yar", externals={'url1': var1, 'url2': var2}))
 
     variable_rules = yara.compile("./rules/variables.yar", externals={'url1': var1, 'url2': var2})
     monkeypatching_rules = yara.compile("./rules/monkey-patching.yar")
