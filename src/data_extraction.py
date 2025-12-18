16e6c4c64e96e5242083dae78626a0694e6924af
denisG99 <gasparollodenis@gmail.com>
2025-12-18 12:40:11 +0100

    features extraction about shadowing and yara

--- src/data_extraction.py
+++ src/data_extraction.py
@@ -7,25 +7,25 @@ import pandas as pd
 
 from detector import Detector
 
-if __name__ == "__main__":
-    pkgs_path = "../data/top10000_pkgs_names.json"
-    temp_dir_path = "./tmp"
-    #fields = ["id", "name", "repository_url"]
+# constants
+TEMP_DIR = "./tmp" # path to the temporary directory where the packages will be downloaded
+PKGS_DATA = "../data/top10000_pkgs_names.json" # path to the json file containing the top n packages names
 
-    if not os.path.exists(pkgs_path):
+if __name__ == "__main__":
+    if not os.path.exists(PKGS_DATA):
         print("Files containing packages doesn't exists!")
 
-    df_pkgs = pd.read_json(pkgs_path)[0]
+    df_pkgs = pd.read_json(PKGS_DATA)[0]
 
     statistics = {} # dictionary to store the gathered statistics for each package under analysis in the top 100
-    local_import = []
-    inner_function = []
+    #local_import = []
+    #inner_function = []
 
     for pkg_name in df_pkgs:
         local_import = []
         inner_function = []
         total_scopes = 0
-        download_path = os.path.join(temp_dir_path, pkg_name)
+        download_path = os.path.join(TEMP_DIR, pkg_name)
 
         # downloading package
         os.system(f"pip install -t {download_path} --no-deps --no-cache-dir {pkg_name}")
@@ -33,8 +33,10 @@ if __name__ == "__main__":
         print(f"Analyzing {pkg_name}...")
         for py_file in pathlib.Path(download_path).glob("**/*.py"): # takes only python files in all possible directories
             detector = Detector(f"{py_file}")
+            shadowing, yara = detector.shadowing_detection()
 
             if detector.get_builder() is not None:
+                # features extraction
                 local_import = detector.local_import_detection()
                 inner_function, scopes_number = detector.inner_function_detection()
                 scope_chain_length = detector.get_builder().length_longest_scope_chain()
@@ -43,7 +45,15 @@ if __name__ == "__main__":
         statistics[pkg_name] = {"local_import": local_import,
                                 "inner_function": inner_function,
                                 "number_of_scopes": total_scopes,
-                                "scope_chain_length": scope_chain_length}
+                                "scope_chain_length": scope_chain_length,
+                                "shadowing": "true" if len(shadowing) > 0 else "false",
+                                "patch_decorator_import": "true" if "patch_decorator_import" in yara else "false",
+                                "patch_decorator_usage": "true" if "patch_decorator_usage" in yara else "false",
+                                "contextmanager_import": "true" if "contextmanager_import" in yara else "false",
+                                "contextmanager_usage": "true" if "contextmanager_usage" in yara else "false",
+                                "with_statement": "true" if "with_statement" in yara else "false",
+                                "overwrite_method_class": "true" if "overwrite_method_class" in yara else "false"}
+
         print(f"Analysis complete")
 
         # removing temp directory, even if isn't empty
