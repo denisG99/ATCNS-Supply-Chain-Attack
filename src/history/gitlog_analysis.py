23f3b5270d6f192446529794b0efbc738d44f206
denisG99 <gasparollodenis@gmail.com>
2026-03-18 10:48:35 +0100

    clone repository command

--- src/history/gitlog_analysis.py
+++ src/history/gitlog_analysis.py
@@ -1,13 +1,24 @@
 import subprocess
 
 UNTIL = "2025-12-31"
+TEMP_DIR = "./tmp"
+
+def get_repo_url(pkg_name: str) -> str:
+    ...
 
 if __name__ == '__main__':
     filename = "../main.py"
+
+
+    # clone repository
+    subprocess.run(["git", "clone", repo_url, str(repo_path)], check=True)
+
+    # get git log result
     try:
         result = subprocess.run(
             [
                 "git",
+                "-C", TEMP_DIR,
                 "log",
                 "--until", UNTIL,
                 "-p", filename
