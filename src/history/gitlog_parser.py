f23c09e36ff7106ad9bf3b9bf2aa2144fd0af49c
denisG99 <gasparollodenis@gmail.com>
2026-03-26 09:25:08 +0100

    FIX diffs type (list to string)

--- src/history/gitlog_parser.py
+++ src/history/gitlog_parser.py
@@ -33,7 +33,7 @@ class GitLogParser:
             author, mail = (aux := re.search(r'Author:\s+(.+)', commit).group(1).split(" "))[0], aux[1].replace("<", "").replace(">", "")
             datetime = re.search(r'Date:\s+(\w{3}\s+\w{3}\s+\d{1,2}\s+\d{2}:\d{2}:\d{2}\s+\d{4})', commit).group(1)
             description = re.search(r'\n\n\s*(.+)\n\n', commit).group(1)
-            diffs = re.split(r'@@\s+-\d+(?:,\d+)?\s+\+\d+(?:,\d+)?\s+@@', commit)[1:]
+            diffs = re.split(r'@@\s+-\d+(?:,\d+)?\s+\+\d+(?:,\d+)?\s+@@', commit)[1]
 
             commits_info[hash] = {"author": author,
                                   "mail": mail,
