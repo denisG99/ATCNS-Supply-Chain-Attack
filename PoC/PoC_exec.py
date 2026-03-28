3048fda0eca52e86c8d7899af809784bb11e4c47
denisG99 <gasparollodenis@gmail.com>
2026-03-28 11:04:23 +0100

    ADD PoC with exec

---
+++ PoC/PoC_exec.py
@@ -0,0 +1,19 @@
+from aux.benign_func import function
+
+def f(x):
+    return x ** 2
+
+if __name__ == "__main__":
+    print(f"Before shadowing: {f(2)}")
+
+    exec("""def f(x):
+    return x ** 3""")
+
+    print(f"After shadowing: {f(2)}")
+
+    function()
+
+    exec("""def function():
+    print('Malicious function')""")
+
+    function()
