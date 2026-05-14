8852258bcb4c7e7e2b525d09dbf7e5dfd17ad3f8
denisG99 <gasparollodenis@gmail.com>
2026-05-14 17:42:50 +0200

    REMOVE dead code

--- yara/main.py
+++ yara/main.py
@@ -5,9 +5,6 @@ if __name__ == "__main__":
     var1 = "prova"
     var2 = "prova"
 
-    rules.append(yara.compile("./rules/monkey-patching.yar"))
-    rules.append(yara.compile("./rules/variables.yar", externals={'url1': var1, 'url2': var2}))
-
     variable_rules = yara.compile("./rules/variables.yar", externals={'url1': var1, 'url2': var2})
     monkeypatching_rules = yara.compile("./rules/monkey-patching.yar")
 
